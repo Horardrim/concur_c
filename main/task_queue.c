da639a17e18b46a2e79021eeb3dd302e61b0766e
lic37 <Chao.Li3@emc.com>
2020-01-24 17:39:31 +0000

    add tests for append_tail and append_head defined in structure task_queue

--- main/task_queue.c
+++ main/task_queue.c
@@ -1,5 +1,101 @@
 #include "task_queue.h"
 
+static int _append_head(task_queue * target_queue, task * insert_task)
+{
+    if (target_queue->_head == NULL &&
+        target_queue->_tail == NULL)
+    {
+        task_node * node = _task_node(insert_task);
+        if (node == NULL)
+        {
+            return 1;
+        }
+
+        target_queue->_head = node;
+        target_queue->_tail = node;
+    }
+    else if (target_queue->_head != NULL &&
+             target_queue->_tail != NULL)
+    {
+        task_node * node = _task_node(insert_task);
+        if (node == NULL)
+        {
+            return 1;
+        }
+
+        node->_next = target_queue->_head;
+        target_queue->_head = node;
+    }
+    else
+    {
+        printf("[WARNING] the situation that either head or tail is NULL shall not happen when adding task from head.\n");
+        return 2;
+    }
+
+    target_queue->_size += 1;
+    return 0;
+}
+
+static int _append_tail(task_queue * target_queue, task * insert_task)
+{
+    if (target_queue->_head == NULL &&
+        target_queue->_tail == NULL)
+    {
+        task_node * node = _task_node(insert_task);
+        if (node == NULL)
+        {
+            return 1;
+        }
+
+        target_queue->_head = node;
+        target_queue->_tail = node;
+    }
+    else if (target_queue->_head != NULL &&
+             target_queue->_tail != NULL)
+    {
+        task_node * node = _task_node(insert_task);
+        if (node == NULL)
+        {
+            return 1;
+        }
+
+        target_queue->_tail->_next = node;
+        target_queue->_tail = node;
+    }
+    else
+    {
+        printf("[WARNING] the situation that either head or tail is NULL shall not happen when adding task from head.\n");
+        return 2;
+    }
+
+    target_queue->_size += 1;
+    return 0;
+}
+
+static void _clear(task_queue * target_queue)
+{
+    if (target_queue->_head == NULL &&
+        target_queue->_tail == NULL)
+    {
+        return;
+    }
+
+    while (target_queue->_head != target_queue->_tail)
+    {
+        task_node * node_remove = target_queue->_head;
+        target_queue->_head = node_remove->_next;
+
+        task_node_(node_remove);
+    }
+
+    task_node * last_node = target_queue->_head;
+    task_node_(last_node);
+
+    target_queue->_head = NULL;
+    target_queue->_tail = NULL;
+    target_queue->_size = 0;
+}
+
 task_queue * _task_queue()
 {
     task_queue * new_task_queue = (task_queue *)malloc(sizeof(task_queue));
@@ -23,100 +119,6 @@ void task_queue_(task_queue * queue)
 {
     queue->clear(queue);
     free(queue);
-}
-
-void _append_head(task_queue * target_queue, task * insert_task)
-{
-    if (target_queue->_head == NULL &&
-        target_queue->_tail == NULL)
-    {
-        task_node * node = _task_node(insert_task);
-        if (node == NULL)
-        {
-            return;
-        }
-
-        target_queue->_head = node;
-        target_queue->_tail = node;
-    }
-    else if (target_queue->_head != NULL &&
-             target_queue->_tail != NULL)
-    {
-        task_node * node = _task_node(insert_task);
-        if (node == NULL)
-        {
-            return;
-        }
-
-        node->_next = target_queue->_head;
-        target_queue->_head = node;
-    }
-    else
-    {
-        printf("[WARNING] the situation that either head or tail is NULL shall not happen when adding task from head.\n");
-        return;
-    }
-
-    target_queue->_size += 1;
-}
-
-void _append_tail(task_queue * target_queue, task * insert_task)
-{
-    if (target_queue->_head == NULL &&
-        target_queue->_tail == NULL)
-    {
-        task_node * node = _task_node(insert_task);
-        if (node == NULL)
-        {
-            return;
-        }
-
-        target_queue->_head = node;
-        target_queue->_tail = node;
-    }
-    else if (target_queue->_head != NULL &&
-             target_queue->_tail != NULL)
-    {
-        task_node * node = _task_node(insert_task);
-        if (node == NULL)
-        {
-            return;
-        }
-
-        target_queue->_tail->_next = node;
-        target_queue->_tail = node;
-    }
-    else
-    {
-        printf("[WARNING] the situation that either head or tail is NULL shall not happen when adding task from head.\n");
-        return;
-    }
-
-    target_queue->_size += 1;
-}
-
-void _clear(task_queue * target_queue)
-{
-    if (target_queue->_head == NULL &&
-        target_queue->_tail == NULL)
-    {
-        return;
-    }
-
-    while (target_queue->_head != target_queue->_tail)
-    {
-        task_node * node_remove = target_queue->_head;
-        task_node_(node_remove);
-
-        target_queue->_head = node_remove->_next;
-    }
-
-    task_node * last_node = target_queue->_head;
-    task_node_(last_node);
-
-    target_queue->_head = NULL;
-    target_queue->_tail = NULL;
-    target_queue->_size = 0;
 }
 
 task_node * _task_node(task * incoming_task)
--- test/task_queue_func_cases.c
+++ test/task_queue_func_cases.c
@@ -6,6 +6,7 @@ extern void run_unit_test(unit_test_func func);
 /**
  * Test name: task_queue_func_case_001
  * Description: initiate an empty task queue. It's head and tail shall be NULL.
+ *              Outage of memory will make this case failed.
  */
 static int task_queue_func_case_001(char * unit_test_func_name)
 {
@@ -13,6 +14,12 @@ static int task_queue_func_case_001(char * unit_test_func_name)
     printf("%s start\n", __FUNCTION__);
 
     task_queue * test_queue = _task_queue();
+
+    if (test_queue == NULL)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
 
     if (test_queue->_head != NULL ||
         test_queue->_tail != NULL)
@@ -28,6 +35,7 @@ static int task_queue_func_case_001(char * unit_test_func_name)
 /**
  * Test name: task_queue_func_case_002
  * Description: create an empty task queue and clean it, no memory issue shall have happened.
+ *              Outage of memory will make this case failed.
  */
 static int task_queue_func_case_002(char * unit_test_func_name)
 {
@@ -36,7 +44,417 @@ static int task_queue_func_case_002(char * unit_test_func_name)
 
     task_queue * test_queue = _task_queue();
 
+    if (test_queue == NULL)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
     test_queue->clear(test_queue);
+    task_queue_(test_queue);
+    return 0;
+}
+
+/**
+ * Test name: task_queue_func_case_003
+ * Description: create an empty task queue and append a task from head, size will increase 1.
+ *              Outage of memory will make this case failed.
+ */
+static int task_queue_func_case_003(char * unit_test_func_name)
+{
+    strncpy(unit_test_func_name, __FUNCTION__, 512);
+    printf("%s start\n", __FUNCTION__);
+
+    task_queue * test_queue = _task_queue();
+    task * test_task = _task(NULL, NULL);
+
+    if (test_queue == NULL || test_task == NULL)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
+    int res = test_queue->append_head(test_queue, test_task);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
+    if (test_queue->_size != 1)
+    {
+        printf("[ERROR] Size shall have been 1 when append a task from head.\n");
+        return 1;
+    }
+
+    if (test_queue->_head->_task != test_task)
+    {
+        printf("[ERROR] pointer of head shall have been test_task.\n");
+        return 1;
+    }
+
+    if (test_queue->_tail->_task != test_task)
+    {
+        printf("[ERROR] pointer of tail shall have been test_task.\n");
+        return 1;
+    }
+
+    task_queue_(test_queue);
+    return 0;
+}
+
+/**
+ * Test name: task_queue_func_case_004
+ * Description: create an empty task queue and append a task from tail, size will increase 1.
+ *              Outage of memory will make this case failed.
+ */
+static int task_queue_func_case_004(char * unit_test_func_name)
+{
+    strncpy(unit_test_func_name, __FUNCTION__, 512);
+    printf("%s start\n", __FUNCTION__);
+
+    task_queue * test_queue = _task_queue();
+    task * test_task = _task(NULL, NULL);
+
+    if (test_queue == NULL || test_task == NULL)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
+    int res = test_queue->append_tail(test_queue, test_task);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
+    if (test_queue->_size != 1)
+    {
+        printf("[ERROR] Size shall have been 1 when append a task from head.\n");
+        return 1;
+    }
+
+    if (test_queue->_head->_task != test_task)
+    {
+        printf("[ERROR] pointer of head shall have been test_task.\n");
+        return 1;
+    }
+
+    if (test_queue->_tail->_task != test_task)
+    {
+        printf("[ERROR] pointer of tail shall have been test_task.\n");
+        return 1;
+    }
+
+    task_queue_(test_queue);
+    return 0;
+}
+
+/**
+ * Test name: task_queue_func_case_005
+ * Description: create an empty task queue and append two tasks from tail, size will be 2.
+ *              Outage of memory will make this case failed.
+ */
+static int task_queue_func_case_005(char * unit_test_func_name)
+{
+strncpy(unit_test_func_name, __FUNCTION__, 512);
+    printf("%s start\n", __FUNCTION__);
+
+    task_queue * test_queue = _task_queue();
+    task * test_task1 = _task(NULL, NULL);
+    task * test_task2 = _task(NULL, NULL);
+
+    if (test_queue == NULL || test_task1 == NULL || test_task2 == NULL)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
+    int res = test_queue->append_tail(test_queue, test_task1);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from tail.\n");
+        return 1;
+    }
+
+    res = test_queue->append_tail(test_queue, test_task2);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from tail second time.\n");
+        return 1;
+    }
+
+    if (test_queue->_head->_task != test_task1)
+    {
+        printf("[ERROR] head pointer shall be test_task1.\n");
+        return 1;
+    }
+
+    if (test_queue->_tail->_task != test_task2)
+    {
+        printf("[ERROR] tail pointer shall be test_task2.\n");
+        return 1;
+    }
+
+    if (test_queue->_size != 2)
+    {
+        printf("[ERROR] Size shall have been 2 when appending 2 tasks from tail.\n");
+        return 1;
+    }
+
+    task_queue_(test_queue);
+    return 0;
+}
+
+/**
+ * Test name: task_queue_func_case_006
+ * Description: create an empty task queue and append two tasks from head, size will be 2.
+ *              Outage of memory will make this case failed.
+ */
+static int task_queue_func_case_006(char * unit_test_func_name)
+{
+strncpy(unit_test_func_name, __FUNCTION__, 512);
+    printf("%s start\n", __FUNCTION__);
+
+    task_queue * test_queue = _task_queue();
+    task * test_task1 = _task(NULL, NULL);
+    task * test_task2 = _task(NULL, NULL);
+
+    if (test_queue == NULL || test_task1 == NULL || test_task2 == NULL)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
+    int res = test_queue->append_head(test_queue, test_task1);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from head .\n");
+        return 1;
+    }
+
+    res = test_queue->append_head(test_queue, test_task2);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from head second time.\n");
+        return 1;
+    }
+
+    if (test_queue->_head->_task != test_task2)
+    {
+        printf("[ERROR] head pointer shall be test_task2.\n");
+        return 1;
+    }
+
+    if (test_queue->_tail->_task != test_task1)
+    {
+        printf("[ERROR] tail pointer shall be test_task1.\n");
+        return 1;
+    }
+
+    if (test_queue->_size != 2)
+    {
+        printf("[ERROR] Size shall have been 2 when appending 2 tasks from head.\n");
+        return 1;
+    }
+
+    task_queue_(test_queue);
+    return 0;
+}
+
+/**
+ * Test name: task_queue_func_case_007
+ * Description: create an empty task queue and append one task from head then append the other from tail, size will be 2.
+ *              Outage of memory will make this case failed.
+ */
+static int task_queue_func_case_007(char * unit_test_func_name)
+{
+strncpy(unit_test_func_name, __FUNCTION__, 512);
+    printf("%s start\n", __FUNCTION__);
+
+    task_queue * test_queue = _task_queue();
+    task * test_task1 = _task(NULL, NULL);
+    task * test_task2 = _task(NULL, NULL);
+
+    if (test_queue == NULL || test_task1 == NULL || test_task2 == NULL)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
+    int res = test_queue->append_head(test_queue, test_task1);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from head .\n");
+        return 1;
+    }
+
+    res = test_queue->append_tail(test_queue, test_task2);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from tail second time.\n");
+        return 1;
+    }
+
+    if (test_queue->_head->_task != test_task1)
+    {
+        printf("[ERROR] head pointer shall be test_task1.\n");
+        return 1;
+    }
+
+    if (test_queue->_tail->_task != test_task2)
+    {
+        printf("[ERROR] tail pointer shall be test_task2.\n");
+        return 1;
+    }
+
+    if (test_queue->_size != 2)
+    {
+        printf("[ERROR] Size shall have been 2 when appending 2 tasks.\n");
+        return 1;
+    }
+
+    task_queue_(test_queue);
+    return 0;
+}
+
+/**
+ * Test name: task_queue_func_case_008
+ * Description: create an empty task queue and append one task from tail then append the other from head, size will be 2.
+ *              Outage of memory will make this case failed.
+ */
+static int task_queue_func_case_008(char * unit_test_func_name)
+{
+strncpy(unit_test_func_name, __FUNCTION__, 512);
+    printf("%s start\n", __FUNCTION__);
+
+    task_queue * test_queue = _task_queue();
+    task * test_task1 = _task(NULL, NULL);
+    task * test_task2 = _task(NULL, NULL);
+
+    if (test_queue == NULL || test_task1 == NULL || test_task2 == NULL)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
+    int res = test_queue->append_tail(test_queue, test_task1);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from tail.\n");
+        return 1;
+    }
+
+    res = test_queue->append_head(test_queue, test_task2);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from head second time.\n");
+        return 1;
+    }
+
+    if (test_queue->_head->_task != test_task2)
+    {
+        printf("[ERROR] head pointer shall be test_task2.\n");
+        return 1;
+    }
+
+    if (test_queue->_tail->_task != test_task1)
+    {
+        printf("[ERROR] tail pointer shall be test_task1.\n");
+        return 1;
+    }
+
+    if (test_queue->_size != 2)
+    {
+        printf("[ERROR] Size shall have been 2 when appending 2 tasks.\n");
+        return 1;
+    }
+
+    task_queue_(test_queue);
+    return 0;
+}
+
+/**
+ * Test name: task_queue_func_case_009
+ * Description: create an empty task queue and append tasks(over 2) randomly from head or tail
+ *              when clean the task queue size shall be 0 also both head and tail pointer shall be NULL
+ *              Outage of memory will make this case failed.
+ */
+static int task_queue_func_case_009(char * unit_test_func_name)
+{
+strncpy(unit_test_func_name, __FUNCTION__, 512);
+    printf("%s start\n", __FUNCTION__);
+
+    task_queue * test_queue = _task_queue();
+    task * test_task1 = _task(NULL, NULL);
+    task * test_task2 = _task(NULL, NULL);
+    task * test_task3 = _task(NULL, NULL);
+
+    if (test_queue == NULL || test_task1 == NULL || test_task2 == NULL || test_task3 == NULL)
+    {
+        printf("[ERROR] memory outage happend.\n");
+        return 1;
+    }
+
+    int res = test_queue->append_tail(test_queue, test_task1);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from head .\n");
+        return 1;
+    }
+
+    res = test_queue->append_head(test_queue, test_task2);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from head second time.\n");
+        return 1;
+    }
+
+    res = test_queue->append_tail(test_queue, test_task3);
+    if (res != 0)
+    {
+        printf("[ERROR] memory outage happend when appending from tail third time.\n");
+        return 1;
+    }
+
+    if (test_queue->_head->_task != test_task2)
+    {
+        printf("[ERROR] head pointer shall be test_task2.\n");
+        return 1;
+    }
+
+    if (test_queue->_head->_next->_task != test_task1)
+    {
+        printf("[ERROR] pointer of middle one shall be test_task1.\n");
+        return 1;
+    }
+
+    if (test_queue->_tail->_task != test_task3)
+    {
+        printf("[ERROR] tail pointer shall be test_task3.\n");
+        return 1;
+    }
+
+    if (test_queue->_size != 3)
+    {
+        printf("[ERROR] Size shall have been 3 when appending 3 tasks.\n");
+        return 1;
+    }
+
+    test_queue->clear(test_queue);
+    if (test_queue->_size != 0)
+    {
+        printf("[ERROR] Size shall have been 0 when clearing task queue.\n");
+        return 1;
+    }
+
+    if (test_queue->_head != NULL || test_queue->_tail != NULL)
+    {
+        printf("[ERROR] both head and tail pointer shall have been NULL.\n");
+        return 1;
+    }
+
     task_queue_(test_queue);
     return 0;
 }
@@ -45,4 +463,11 @@ void run_task_queue_func_cases()
 {
     run_unit_test(task_queue_func_case_001);
     run_unit_test(task_queue_func_case_002);
+    run_unit_test(task_queue_func_case_003);
+    run_unit_test(task_queue_func_case_004);
+    run_unit_test(task_queue_func_case_005);
+    run_unit_test(task_queue_func_case_006);
+    run_unit_test(task_queue_func_case_007);
+    run_unit_test(task_queue_func_case_008);
+    run_unit_test(task_queue_func_case_009);
 }
